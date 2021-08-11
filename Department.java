b223f99f7a575709481d61633e9a13b5614fdb20
unknown <bskr.2020@gmail.com>
2021-08-11 06:24:22 +0530

    adding new files

---
+++ Department.java
@@ -0,0 +1,10 @@
+package com.org.first;
+
+public class Department {
+
+	public static void main(String[] args) {
+		System.out.println("Hello this is my second message");
+
+	}
+
+}
---
+++ Hellostudent.java
@@ -0,0 +1,10 @@
+package com.org.first;
+
+public class Hellostudent {
+
+	public static void main(String[] args) {
+		System.out.println("Hello this is my first message");
+
+	}
+
+}
